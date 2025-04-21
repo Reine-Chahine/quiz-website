81cfb63a395d0749094f56c8ca867b0152b3221a
Reine-Chahine <reinechahine3@gmail.com>
2025-04-21 13:24:02 +0300

    rendering quize

--- home.js
+++ home.js
@@ -9,3 +9,18 @@ const sampleQuizzes = [
   
   const quizzes = JSON.parse(localStorage.getItem("quizzes") || "[]");
   const quizList = document.getElementById("quizList");
+  quizzes.forEach(quiz => {
+    const div = document.createElement("div");
+    div.className = "quiz-item";
+    div.innerText = quiz.title;
+    div.onclick = () => {
+      if (quiz.title === "General Knowledge") {
+        window.location.href = "web.html";
+      } else if (quiz.title === "Science & Nature") {
+        window.location.href = "css.html";
+      } else if (quiz.title === "History") {
+        window.location.href = "javascript.html";
+      }
+    };
+    quizList.appendChild(div);
+  });
