c091aaac5d52d51b771844340e1d29515071f2ed
Reine-Chahine <reinechahine3@gmail.com>
2025-04-21 13:22:11 +0300

    loading quizes in local storage

--- home.js
+++ home.js
@@ -3,3 +3,9 @@ const sampleQuizzes = [
     { id: 2, title: "CSS Quiz" },
     { id: 3, title: "JavaScript Quiz" }
   ];
+  if (!localStorage.getItem("quizzes")) {
+    localStorage.setItem("quizzes", JSON.stringify(sampleQuizzes));
+  }
+  
+  const quizzes = JSON.parse(localStorage.getItem("quizzes") || "[]");
+  const quizList = document.getElementById("quizList");
